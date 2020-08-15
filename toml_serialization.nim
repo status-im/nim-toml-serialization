73c1cd35858090adfb582f8b6f57b3dab7e9d3a1
jangko <jangko128@gmail.com>
2020-08-15 20:51:47 +0700

    decoder: improve api

--- toml_serialization.nim
+++ toml_serialization.nim
@@ -90,15 +90,12 @@ template saveFile*(_: type Toml,
 import
   stew/shims/macros
 
-template decode*(_: type Toml,
-                 input: string,
-                 RecordType: distinct type,
-                 key: string,
-                 tomlCase: TomlCase = TomlCaseSensitive,
-                 params: varargs[untyped]): auto =
+template tomlDecodeImpl*(input: untyped,
+                         RecordType: distinct type,
+                         key: string,
+                         tomlCase: TomlCase,
+                         params: varargs[untyped]): auto =
 
-  # TODO, this is duplicated only due to a Nim bug:
-  # If `input` was `string|openarray[byte]`, it won't match `seq[byte]`
   mixin init, ReaderType
   {.noSideEffect.}:
     # We assume that there are no side-effects here, because we are
@@ -113,35 +110,34 @@ template decode*(_: type Toml,
     except IOError:
       raise (ref Defect)() # memory inputs cannot raise an IOError
 
-template decode*(_: type Toml,
-                 input: openarray[byte],
+template decode*(_: type Toml, input: string,
                  RecordType: distinct type,
-                 key: string,
-                 tomlCase: TomlCase = TomlCaseSensitive,
+                 key: string, tomlCase: TomlCase,
                  params: varargs[untyped]): auto =
+  tomlDecodeImpl(input, RecordType, key, tomlCase, params)
 
-  # TODO, this is duplicated only due to a Nim bug:
-  # If `input` was `string|openarray[byte]`, it won't match `seq[byte]`
-  mixin init, ReaderType
-  {.noSideEffect.}:
-    # We assume that there are no side-effects here, because we are
-    # using a `memoryInput`. The computed side-effects are coming
-    # from the fact that the dynamic dispatch mechanisms used in
-    # faststreams may be reading from a file or a network device.
-    try:
-      var stream = unsafeMemoryInput(input)
-      var reader = unpackArgs(init, [TomlReader, stream, params])
-      reader.moveToKey(key, tomlCase)
-      reader.readValue(RecordType)
-    except IOError:
-      raise (ref Defect)() # memory inputs cannot raise an IOError
+template decode*(_: type Toml, input: string,
+                 RecordType: distinct type,
+                 key: string, params: varargs[untyped]): auto =
+  tomlDecodeImpl(input, RecordType, key, TomlCaseSensitive, params)
 
-template loadFile*(Format: distinct type,
-                   filename: string,
-                   RecordType: distinct type,
-                   key: string,
-                   tomlCase: TomlCase = TomlCaseSensitive,
-                   params: varargs[untyped]): auto =
+template decode*(_: type Toml, input: openarray[byte],
+                 RecordType: distinct type,
+                 key: string, tomlCase: TomlCase,
+                 params: varargs[untyped]): auto =
+  tomlDecodeImpl(input, RecordType, key, tomlCase, params)
+
+template decode*(_: type Toml, input: openarray[byte],
+                 RecordType: distinct type,
+                 key: string, params: varargs[untyped]): auto =
+  tomlDecodeImpl(input, RecordType, key, TomlCaseSensitive, params)
+
+
+template tomlLoadImpl*(filename: string,
+                       RecordType: distinct type,
+                       key: string, tomlCase: TomlCase,
+                       params: varargs[untyped]): auto =
+
   mixin init, ReaderType, readValue
 
   var stream = memFileInput(filename)
@@ -151,3 +147,14 @@ template loadFile*(Format: distinct type,
     reader.readValue(RecordType)
   finally:
     close stream
+
+template loadFile*(_: type Toml, filename: string,
+                   RecordType: distinct type,
+                   key: string, tomlCase: TomlCase,
+                   params: varargs[untyped]): auto =
+  tomlLoadImpl(filename, RecordType, key, tomlCase, params)
+
+template loadFile*(_: type Toml, filename: string,
+                   RecordType: distinct type,
+                   key: string, params: varargs[untyped]): auto =
+  tomlLoadImpl(filename, RecordType, key, TomlCaseSensitive, params)
