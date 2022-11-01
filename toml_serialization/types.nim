ec908e4134dd570b5c1bda75dccee2ff63b105be
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2022-11-01 08:25:53 +0100

    func instead of annotated proc

--- toml_serialization/types.nim
+++ toml_serialization/types.nim
@@ -126,9 +126,9 @@ when tomlOrderedTable:
       let value {.inject.} = addr(valx)
       body1
 
-proc `==`*(a, b: TomlValueRef): bool {.noSideEffect.} 
+func `==`*(a, b: TomlValueRef): bool 
 
-proc `==`*(a, b: TomlTableRef): bool {.noSideEffect.} =
+func `==`*(a, b: TomlTableRef): bool =
   result = true
   if a.len != b.len:
     return false
@@ -141,7 +141,7 @@ proc `==`*(a, b: TomlTableRef): bool {.noSideEffect.} =
 
   result = a[] == b[]
 
-proc `==`*(a, b: TomlValueRef): bool {.noSideEffect.} =
+func `==`*(a, b: TomlValueRef): bool =
   const
     tableKind = {TomlKind.Table, TomlKind.InlineTable}
 
